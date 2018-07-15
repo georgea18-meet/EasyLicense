ac624a6af98a4419e87072a3a9fbeab35e34382f
George Abu Dauod <abudauodg@gmail.com>
2018-07-15 23:43:20 +0300

    Updated the StudentSignup function

--- static/script.js
+++ static/script.js
@@ -11,7 +11,33 @@ console.log(app.options.credential === config.credential);  // true
 console.log(app.options.databaseURL === config.databaseURL);  // true
 
 auth = firebase.auth(app);
+db = firebase.database(app);
+databaseRef = db.ref();
 
-function signup(email,password){
-    auth.createUserWithEmailAndPassword(email,password);
+function StudentSignup(email,password,first_name,last_name,age){
+    if (auth.currentUser==null){
+        auth.createUserWithEmailAndPassword(email,password);
+        var student = {
+            first_name: first_name,
+            last_name: last_name,
+            age: age,
+            hours: 0,
+            lessons: 0,
+            money_paid: 0,
+            total_money: 0
+        };
+        databaseRef.child("students").child(auth.currentUser.uid).set(student);
+        console.log(auth.currentUser.uid);
+        console.log(auth.currentUser.email);}
+    else{
+        console.log(auth.currentUser.email+" is signed in!");
+    }
+}
+
+function signin(email,password){
+    auth.signInWithEmailAndPassword(email,password);
+    if (auth.currentUser!=null){
+        console.log(auth.currentUser.uid);
+        console.log(auth.currentUser.email);
+    }
 }
